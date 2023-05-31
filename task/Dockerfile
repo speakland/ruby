dfcacbd188ff1831ad97bf3f5588285b70d945ea
Mark Makarenko <91777760+speakland@users.noreply.github.com>
2023-05-31 18:05:15 +0300

    Add files via upload

---
+++ task/Dockerfile
@@ -0,0 +1,7 @@
+FROM ruby:latest
+
+COPY ruby.rb /app/ruby.rb
+
+WORKDIR /app
+
+CMD ["ruby", "ruby.rb"]
---
+++ task/ruby.rb
@@ -0,0 +1,8 @@
+names = ["Alice", "Bob", "Eve", "David", "Charlie"]  # Пример строкового массива имён
+
+sorted_names = names.sort.reverse
+
+puts "Имена в алфавитном порядке по убыванию:"
+sorted_names.each do |name|
+  puts name
+end
